509390429072f012d8306611c6038eef14379fb6
Thomas Cheng <thomascheng81@gmail.com>
2017-08-14 20:34:59 -0400

    Add click to get a random quote

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,33 @@ import Quote from "./Quote";
 import random from "lodash/random";
 
 const Container = styled.div`
-  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   width: 100%;
   max-width: 600px;
   margin: 0 auto;
   padding: 0 20px;
   box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  height: 100%;
-  
+
   @media (min-width: 400px) {
     width: 85%;
     padding: 0;
   }
+`;
+
+const QuoteContainer = styled.div`
+  padding: 15px 0;
+  flex-grow: 1;
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  position: relative;
 `;
 
 class App extends Component {
@@ -28,19 +41,28 @@ class App extends Component {
         author: PropTypes.shape({
           name: PropTypes.string.isRequired,
           time: PropTypes.string,
-          position: PropTypes.string,
+          position: PropTypes.string
         }).isRequired,
         quote: PropTypes.string.isRequired
       })
     ).isRequired
   };
 
-  constructor (props) {
+  constructor(props) {
     super();
 
-    const currentQuoteIndex = random(0, props.quotes.length - 1);
-    this.state = { currentQuoteIndex };
+    this.state = {
+      currentQuoteIndex: props.quotes.length - 1
+    };
   }
+
+  handleClick = evt => {
+    evt.preventDefault();
+
+    this.setState({
+      currentQuoteIndex: random(0, this.props.quotes.length - 1)
+    });
+  };
 
   render() {
     const { quotes } = this.props;
@@ -49,7 +71,9 @@ class App extends Component {
 
     return (
       <Container>
-        <Quote quote={currentQuote} />
+        <QuoteContainer onClick={this.handleClick}>
+          <Quote quote={currentQuote} />
+        </QuoteContainer>
       </Container>
     );
   }

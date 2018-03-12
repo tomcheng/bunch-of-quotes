e00bd7574d954b9603d9dcc2092334f16172e5f1
Thomas Cheng <thomascheng81@gmail.com>
2018-03-11 21:48:22 -0400

    Get tab to select next open letter

--- src/components/Cryptogram.js
+++ src/components/Cryptogram.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import flatMap from "lodash/flatMap";
+import findIndex from "lodash/findIndex";
 import { generateCipher, applyCipher } from "../utils/cipher";
 import { simpleMemoize } from "../utils/functionUtils";
+import { alphabet } from "../utils/constants";
 import Word from "./Word";
 
 const Container = styled.div`
@@ -38,8 +40,22 @@ class Cryptogram extends Component {
     });
   };
 
-  handleChange = evt => {
-    const guess = evt.target.value.toUpperCase();
+  handleKeyDown = evt => {
+    evt.preventDefault();
+    const { key } = evt;
+
+    const nextOpenId = this.getNextOpenId();
+
+    if (key === "Tab") {
+      this.setState({ selectedLetterId: nextOpenId });
+      return;
+    }
+
+    if (!alphabet.split("").includes(key.toUpperCase())) {
+      return;
+    }
+
+    const guess = evt.key.toUpperCase();
     const selectedLetter = this.getSelectedLetter();
 
     this.setState(state => ({
@@ -49,6 +65,19 @@ class Cryptogram extends Component {
         [selectedLetter]: guess
       }
     }));
+  };
+
+  getNextOpenId = () => {
+    const { text } = this.props;
+    const { selectedLetterId, cipher, guesses } = this.state;
+
+    const words = this.getWords(text, cipher);
+    const letters = flatMap(words, word => word.letters);
+    const selectedIndex = findIndex(letters, letter => letter.id === selectedLetterId);
+    const rearrangedLetters = letters.slice(selectedIndex + 1).concat(letters.slice(0, selectedIndex + 1));
+    const nextOpenLetter = rearrangedLetters.find(letter => !guesses[letter.letter]);
+
+    return nextOpenLetter ? nextOpenLetter.id : null;
   };
 
   getSelectedLetter = () => {
@@ -107,7 +136,7 @@ class Cryptogram extends Component {
           }}
           type="text"
           value=""
-          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
         />
       </Container>
     );
